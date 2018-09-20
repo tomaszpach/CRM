f455b48778a1402b83d33a3ed8253d79150f1247
Tomasz Pach <tomaszpach@icloud.com>
2018-09-20 15:51:54 +0200

    first step

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-jQuery('#rootwizard').bootstrapWizard({
-    'tabClass': 'form-wizard',
-    'onNext': function(tab, navigation, index) {
-        var $valid = $("#commentForm").valid();
-        if(!$valid) {
-            $validator.focusInvalid();
-            return false;
+// todo optimize scripts!
+jQuery(document).ready(function() {
+    $('#rootwizard .finish').click(function() {
+        alert('Finished!, Starting over!');
+        $('#rootwizard').find("a[href*='tab1']").trigger('click');
+    });
+
+    $('#rootwizard').bootstrapWizard({
+        'tabClass': 'form-wizard',
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            console.clear();
+            console.log('total: ', $total);
+            console.log('current:', $current);
+            console.log('percent', $percent + '%');
+
+            $('#rootwizard').find('#bar .progress-bar').css({width:$percent+'%'});
+            // console.log($('#rootwizard').find('#bar .progress-bar'));
+
+            if ($percent === 100) {
+                $('#rootwizard').find('.pager .finish').css('display', 'inline-block');
+            } else {
+                $('#rootwizard').find('.pager .finish').hide();
+            }
         }
-        else{
-            $('#rootwizard').find('.form-wizard').children('li').eq(index-1).addClass('complete');
-            $('#rootwizard').find('.form-wizard').children('li').eq(index-1).find('.step').html('<i class="icon-ok"></i>');
-        }
-    }
+    });
 });
