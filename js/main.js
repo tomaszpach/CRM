ed1733ab8b7c14304225129c29dae421e3b91fda
Tomasz Pach <tomaszpach@icloud.com>
2018-10-01 09:25:27 +0200

    dashboard boxes - charts grid

--- js/main.js
+++ js/main.js
@@ -1,31 +1,14 @@
 jQuery(document).ready(function () {
-    let $rootWizard = $('#rootwizard'),
-        $pager = $rootWizard.find('.pager'),
-        $finish = $pager.find('.finish'),
-        $next = $pager.find('.next');
+    var initAnimation = function(){
+        $('[data-init="animate-number"], .animate-number').each(function () {
+            var data = $(this).data();
+            $(this).animateNumbers(data.value, true, parseInt(data.animationDuration, 10));
+        });
+        $('[data-init="animate-progress-bar"], .animate-progress-bar').each(function () {
+            var data = $(this).data();
+            $(this).css('width', data.percentage);
+        });
+    };
 
-    $rootWizard.find('.finish').click(function () {
-        // todo do something on finish
-        alert('Finished!, Starting over!');
-        $rootWizard.find("a[href*='tab1']").trigger('click');
-    });
-
-    $rootWizard.bootstrapWizard({
-        'tabClass': 'form-wizard',
-        onTabShow: function (tab, navigation, index) {
-            let $total = navigation.find('li').length,
-                $current = index + 1,
-                $percent = ($current / $total) * 100;
-
-            $rootWizard.find('#bar .progress-bar').css({width: $percent + '%'});
-
-            if ($percent === 100) {
-                $finish.show();
-                $next.hide();
-            } else {
-                $finish.hide();
-                $next.show();
-            }
-        }
-    });
+    initAnimation();
 });
