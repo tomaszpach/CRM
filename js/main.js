a11d89bd1b0216dc99b9321b4b1f62d08b4b8055
Tomasz Pach <tomaszpach@icloud.com>
2018-10-02 15:04:41 +0200

    cleanup, optimize script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    var graph;
-    var rick;
+    let graph;
+    let rick;
 
     initAnimateNumbers('animate-number');
     loadServerChart();
@@ -19,123 +19,49 @@ $(document).ready(function () {
         }
     }
 
-
-    $("#mini-chart-orders").sparkline([1,4,6,2,0,5,6,4], {
+    // Sparkline charts
+    $("#mini-chart-orders").sparkline([1, 4, 6, 2, 0, 5, 6, 4], {
         type: 'bar',
         height: '30px',
         barWidth: 6,
         barSpacing: 2,
         barColor: '#f35958',
-        negBarColor: '#f35958'});
+        negBarColor: '#f35958'
+    });
 
-    $("#mini-chart-other").sparkline([1,4,6,2,0,5,6,4], {
+    $("#mini-chart-other").sparkline([1, 4, 6, 2, 0, 5, 6, 4], {
         type: 'bar',
         height: '30px',
         barWidth: 6,
         barSpacing: 2,
         barColor: '#0aa699',
-        negBarColor: '#0aa699'});
+        negBarColor: '#0aa699'
+    });
 
     $('#ram-usage').easyPieChart({
-        lineWidth:9,
-        barColor:'#f35958',
-        trackColor:'#e5e9ec',
-        scaleColor:false
+        lineWidth: 9,
+        barColor: '#f35958',
+        trackColor: '#e5e9ec',
+        scaleColor: false
     });
 
     $('#disk-usage').easyPieChart({
-        lineWidth:9,
-        barColor:'#7dc6ec',
-        trackColor:'#e5e9ec',
-        scaleColor:false
+        lineWidth: 9,
+        barColor: '#7dc6ec',
+        trackColor: '#e5e9ec',
+        scaleColor: false
     });
 
-    var loadRickshaw = function() {
-        var seriesData = [ [], [],[]];
-        var random = new Rickshaw.Fixtures.RandomData(50);
+    //Ricksaw Chart for Server Load - Autoupdate
+    function loadServerChart() {
+        let seriesData = [[], []];
+        let random = new Rickshaw.Fixtures.RandomData(50);
 
-        for (var i = 0; i < 50; i++) {
+        for (let i = 0; i < 50; i++) {
             random.addData(seriesData);
         }
 
-        rick = new Rickshaw.Graph( {
-            element: document.querySelector("#ricksaw"),
-            height: 200,
-            renderer: 'area',
-            series: [
-                {
-                    data: seriesData[0],
-                    color: '#736086',
-                    name:'Downloads'
-                },{
-                    data: seriesData[1],
-                    color: '#f8a4a3',
-                    name:'Uploads'
-                },
-                {
-                    data: seriesData[2],
-                    color: '#eceff1',
-                    name:'All'
-                }
-            ]
-        } );
-        var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-            graph: rick
-        });
-
-        random.addData(seriesData);
-        rick.update();
-
-        var ticksTreatment = 'glow';
-
-        var xAxis = new Rickshaw.Graph.Axis.Time( {
-            graph: rick,
-            ticksTreatment: ticksTreatment,
-            timeFixture: new Rickshaw.Fixtures.Time.Local()
-        });
-
-        xAxis.render();
-
-        var yAxis = new Rickshaw.Graph.Axis.Y( {
-            graph: rick,
-            tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-            ticksTreatment: ticksTreatment
-        });
-
-        var legend = new Rickshaw.Graph.Legend( {
-            graph: rick,
-            element: document.getElementById('legend')
-        });
-
-        yAxis.render();
-
-        var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
-            graph: rick,
-            legend: legend
-        } );
-
-        var order = new Rickshaw.Graph.Behavior.Series.Order( {
-            graph: rick,
-            legend: legend
-        } );
-
-        var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight( {
-            graph: rick,
-            legend: legend
-        } );
-    };
-
-
-//Ricksaw Chart for Server Load - Autoupdate
-    function loadServerChart(){
-        var seriesData = [ [], []];
-        var random = new Rickshaw.Fixtures.RandomData(50);
-
-        for (var i = 0; i < 50; i++) {
-            random.addData(seriesData);
-        }
-
-        graph = new Rickshaw.Graph( {
+        graph = new Rickshaw.Graph({
             element: document.querySelector("#chart"),
             height: 200,
             renderer: 'area',
@@ -143,37 +69,36 @@ $(document).ready(function () {
                 {
                     data: seriesData[0],
                     color: 'rgba(0,144,217,0.51)',
-                    name:'DB Server'
-                },{
+                    name: 'DB Server'
+                }, {
                     data: seriesData[1],
                     color: '#eceff1',
-                    name:'Web Server'
+                    name: 'Web Server'
                 }
             ]
-        } );
-        var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        });
+        new Rickshaw.Graph.HoverDetail({
             graph: graph
         });
 
-        setInterval( function() {
+        setInterval(function () {
             random.removeData(seriesData);
             random.addData(seriesData);
             graph.update();
 
-        },1000);
+        }, 1000);
     }
 
-    //Ricksaw Chart Sample
+    // Ricksaw Chart Sample
+    function loadSampleChart() {
+        let seriesData = [[], [], []];
+        let random = new Rickshaw.Fixtures.RandomData(50);
 
-    function loadSampleChart(){
-        var seriesData = [ [], [],[]];
-        var random = new Rickshaw.Fixtures.RandomData(50);
-
-        for (var i = 0; i < 50; i++) {
+        for (let i = 0; i < 50; i++) {
             random.addData(seriesData);
         }
 
-        rick = new Rickshaw.Graph( {
+        rick = new Rickshaw.Graph({
             element: document.querySelector("#ricksaw"),
             height: 200,
             renderer: 'area',
@@ -181,29 +106,29 @@ $(document).ready(function () {
                 {
                     data: seriesData[0],
                     color: '#736086',
-                    name:'Downloads'
-                },{
+                    name: 'Downloads'
+                }, {
                     data: seriesData[1],
                     color: '#f8a4a3',
-                    name:'Uploads'
+                    name: 'Uploads'
                 },
                 {
                     data: seriesData[2],
                     color: '#eceff1',
-                    name:'All'
+                    name: 'All'
                 }
             ]
-        } );
-        var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        });
+        new Rickshaw.Graph.HoverDetail({
             graph: rick
         });
 
         random.addData(seriesData);
         rick.update();
 
-        var ticksTreatment = 'glow';
+        let ticksTreatment = 'glow';
 
-        var xAxis = new Rickshaw.Graph.Axis.Time( {
+        let xAxis = new Rickshaw.Graph.Axis.Time({
             graph: rick,
             ticksTreatment: ticksTreatment,
             timeFixture: new Rickshaw.Fixtures.Time.Local()
@@ -211,40 +136,40 @@ $(document).ready(function () {
 
         xAxis.render();
 
-        var yAxis = new Rickshaw.Graph.Axis.Y( {
+        let yAxis = new Rickshaw.Graph.Axis.Y({
             graph: rick,
             tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
             ticksTreatment: ticksTreatment
         });
 
-        var legend = new Rickshaw.Graph.Legend( {
+        let legend = new Rickshaw.Graph.Legend({
             graph: rick,
             element: document.getElementById('legend')
         });
 
         yAxis.render();
 
-        var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
+        new Rickshaw.Graph.Behavior.Series.Toggle({
             graph: rick,
             legend: legend
-        } );
+        });
 
-        var order = new Rickshaw.Graph.Behavior.Series.Order( {
+        new Rickshaw.Graph.Behavior.Series.Order({
             graph: rick,
             legend: legend
-        } );
+        });
 
-        var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight( {
+        new Rickshaw.Graph.Behavior.Series.Highlight({
             graph: rick,
             legend: legend
-        } );
+        });
     }
 
 
     //Weahter Icons
-    var loadAnimatedWeatherIcons = function(){
+    let loadAnimatedWeatherIcons = function () {
         /*** Animated Weather Icon **/
-        var icons = new Skycons({"color": "white"});
+        let icons = new Skycons({"color": "white"});
         icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
         icons.set("wind", Skycons.WIND);
         icons.play();
@@ -254,7 +179,7 @@ $(document).ready(function () {
 
 
     //Jquery vector map
-    var cityAreaData = [
+    let cityAreaData = [
         500.70,
         410.16,
         210.69,
@@ -264,21 +189,22 @@ $(document).ready(function () {
         130.22,
         120.71,
         100.32
-    ]
-    if ($.fn.vectorMap){
+    ];
+
+    if ($.fn.vectorMap) {
         $('#world-map').vectorMap({
             map: 'us_lcc_en',
             scaleColors: ['#C8EEFF', '#0071A4'],
             normalizeFunction: 'polynomial',
-            focusOn:{
+            focusOn: {
                 x: 5,
                 y: 1,
                 scale: 1.8
             },
-            zoomOnScroll:false,
-            zoomMin:0.85,
+            zoomOnScroll: false,
+            zoomMin: 0.85,
             hoverColor: false,
-            regionStyle:{
+            regionStyle: {
                 initial: {
                     fill: '#a5ded9',
                     "fill-opacity": 1,
@@ -292,8 +218,7 @@ $(document).ready(function () {
                 selected: {
                     fill: 'yellow'
                 },
-                selectedHover: {
-                }
+                selectedHover: {}
             },
             markerStyle: {
                 initial: {
@@ -311,15 +236,14 @@ $(document).ready(function () {
                 selected: {
                     fill: 'blue'
                 },
-                selectedHover: {
-                }
+                selectedHover: {}
             },
             backgroundColor: '#ffffff',
-            markers :[
+            markers: [
                 {latLng: [30.22, -81.88], name: 'Cecil,FL'},
-                {latLng: [25.8,-80.28], name: 'Miami Intl,FL'},
+                {latLng: [25.8, -80.28], name: 'Miami Intl,FL'},
                 {latLng: [32.33, -85.00], name: 'Fort Benning,GA'},
-                {latLng: [34.35, -85.17 ], name: 'Rome/Russell,GA'},
+                {latLng: [34.35, -85.17], name: 'Rome/Russell,GA'},
                 {latLng: [35.90, -82.82], name: 'Hot Springs,NC'},
                 {latLng: [35.85, -77.88], name: 'Rocky Mt,NC'},
                 {latLng: [32.90, -97.03], name: 'Dallas/FW,TX'},
