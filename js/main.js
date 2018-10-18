efa86690a28396b717a22e70e283f5a768bbee2b
tomaszpach <tomaszpach@icloud.com>
2018-10-18 21:38:07 +0200

    wykresy na bazie informacji z maila od Marcina

--- js/main.js
+++ js/main.js
@@ -189,12 +189,18 @@ $(document).ready(function () {
         negBarColor: '#0aa699'
     });
 
+    // todo usun console logi
+    // todo Zastanwo sie nad dokladnoscia procentow (czesc wykresow wymaga dokladnosci do setnych)
+    // todo dodaj mozliwosc opisywania wykresow (tak jak to jest w przykladzie na mailu)
+    // todo moze warto przeniesc ustawienia kolorow do HTML i wyciaganie ich tutaj?
+    // todo dwa ostatnie wykresy? co to jest?!
     const setupEasyPieChart = (DOMElement, config) => {
         const $element = document.getElementById(DOMElement),
             $pieTextWrapper = $element.getElementsByClassName('easy-pie-percent')[0],
             $pieTextSpan = $pieTextWrapper.getElementsByTagName('span'),
             ramUsageDataset = $element.dataset;
 
+        console.log($element.style);
         console.log($pieTextSpan);
         console.log($pieTextWrapper);
 
@@ -209,26 +215,38 @@ $(document).ready(function () {
         $pieTextSpan[0].innerText = fixedPercent;
         $pieTextSpan[1].innerText = getSpanText;
 
-        jQuery('#' + DOMElement).easyPieChart(config);
-        jQuery('#' + DOMElement).data('easyPieChart').update(fixedPercent);
+        const $DOMElement = jQuery('#' + DOMElement);
+
+        $DOMElement.easyPieChart(config);
+        $DOMElement.data('easyPieChart').update(fixedPercent);
     };
 
     const ramUsageConfig = {
         lineWidth: 9,
-        barColor: '#f35958',
+        barColor: '#ff8e4b',
         trackColor: '#e5e9ec',
         scaleColor: false
     };
 
     const diskUsageConfig = {
         lineWidth: 9,
-        barColor: '#7dc6ec',
+        barColor: '#f43730',
+        trackColor: '#e5e9ec',
+        scaleColor: false
+    };
+
+    const konwersjaConfig = {
+        lineWidth: 9,
+        barColor: '#5ec65e',
         trackColor: '#e5e9ec',
         scaleColor: false
     };
 
     setupEasyPieChart('ram-usage', ramUsageConfig);
     setupEasyPieChart('disk-usage', diskUsageConfig);
+    setupEasyPieChart('konwersja', konwersjaConfig);
+    setupEasyPieChart('nieskontraktowane', diskUsageConfig);
+    setupEasyPieChart('utracone', diskUsageConfig);
 
     //Ricksaw Chart for Server Load - Autoupdate
     function loadServerChart() {
@@ -441,82 +459,82 @@ $(document).ready(function () {
         });
     }
 
-    if ($.fn.vectorMap) {
-        var plants = [
-            {name: 'VAK', coords: [50.0091294, 9.0371812], status: 'closed', offsets: [0, 2]},
-            {name: 'MZFR', coords: [49.0543102, 8.4825862], status: 'closed', offsets: [0, 2]},
-            {name: 'AVR', coords: [50.9030599, 6.4213693], status: 'closed'},
-            {name: 'KKR', coords: [53.1472465, 12.9903674], status: 'closed'},
-            {name: 'KRB', coords: [48.513264, 10.4020357], status: 'activeUntil2018'},
-            {name: 'KWO', coords: [49.364503, 9.076252], status: 'closed'},
-            {name: 'KWL', coords: [52.5331853, 7.2505223], status: 'closed', offsets: [0, -2]},
-            {name: 'HDR', coords: [50.1051446, 8.9348691], status: 'closed', offsets: [0, -2]},
-            {name: 'KKS', coords: [53.6200685, 9.5306289], status: 'closed'},
-            {name: 'KKN', coords: [48.6558015, 12.2500848], status: 'closed', offsets: [0, -2]},
-            {name: 'KGR', coords: [54.1417497, 13.6583877], status: 'closed'},
-            {name: 'KWB', coords: [49.709331, 8.415865], status: 'closed'},
-            {name: 'KWW', coords: [51.6396481, 9.3915617], status: 'closed'},
-            {name: 'GKN', coords: [49.0401151, 9.1721088], status: 'activeUntil2022'},
-            {name: 'KKB', coords: [53.8913533, 9.2005777], status: 'closed', offsets: [0, -5]},
-            {name: 'KKI', coords: [48.5544748, 12.3472095], status: 'activeUntil2022', offsets: [0, 2]},
-            {name: 'KKU', coords: [53.4293465, 8.4774649], status: 'closed'},
-            {name: 'KNK', coords: [49.1473279, 8.3827739], status: 'closed'},
-            {name: 'KKP', coords: [49.2513078, 8.4356761], status: 'activeUntil2022', offsets: [0, -2]},
-            {name: 'KKG', coords: [49.9841308, 10.1846373], status: 'activeUntil2018'},
-            {name: 'KKK', coords: [53.4104656, 10.4091597], status: 'closed'},
-            {name: 'KWG', coords: [52.0348748, 9.4097793], status: 'activeUntil2022'},
-            {name: 'KBR', coords: [53.850666, 9.3457603], status: 'closed', offsets: [0, 5]},
-            {name: 'KMK', coords: [50.408791, 7.4861956], status: 'closed'},
-            {name: 'THTR', coords: [51.6786228, 7.9700232], status: 'closed'},
-            {name: 'KKE', coords: [52.4216974, 7.3706389], status: 'activeUntil2022', offsets: [0, 2]}
-        ];
-
-        new jvm.Map({
-            container: $('#world-map2'),
-            map: 'de_merc',
-            markers: plants.map(function (h) {
-                return {name: h.name, latLng: h.coords}
-            }),
-            labels: {
-                markers: {
-                    render: function (index) {
-                        return plants[index].name;
-                    },
-                    offsets: function (index) {
-                        var offset = plants[index]['offsets'] || [0, 0];
-
-                        return [offset[0] - 7, offset[1] + 3];
-                    }
-                }
-            },
-            series: {
-                markers: [{
-                    attribute: 'image',
-                    scale: {
-                        'closed': '/img/icon-np-3.png',
-                        'activeUntil2018': '/img/icon-np-2.png',
-                        'activeUntil2022': '/img/icon-np-1.png'
-                    },
-                    values: plants.reduce(function (p, c, i) {
-                        p[i] = c.status;
-                        return p
-                    }, {}),
-                    legend: {
-                        horizontal: true,
-                        title: 'Nuclear power station status',
-                        labelRender: function (v) {
-                            return {
-                                closed: 'Closed',
-                                activeUntil2018: 'Scheduled for shut down<br> before 2018',
-                                activeUntil2022: 'Scheduled for shut down<br> before 2022'
-                            }[v];
-                        }
-                    }
-                }]
-            }
-        });
-
-
-        $('#world-map').vectorMap({})
-    }
+    // if ($.fn.vectorMap) {
+    //     var plants = [
+    //         {name: 'VAK', coords: [50.0091294, 9.0371812], status: 'closed', offsets: [0, 2]},
+    //         {name: 'MZFR', coords: [49.0543102, 8.4825862], status: 'closed', offsets: [0, 2]},
+    //         {name: 'AVR', coords: [50.9030599, 6.4213693], status: 'closed'},
+    //         {name: 'KKR', coords: [53.1472465, 12.9903674], status: 'closed'},
+    //         {name: 'KRB', coords: [48.513264, 10.4020357], status: 'activeUntil2018'},
+    //         {name: 'KWO', coords: [49.364503, 9.076252], status: 'closed'},
+    //         {name: 'KWL', coords: [52.5331853, 7.2505223], status: 'closed', offsets: [0, -2]},
+    //         {name: 'HDR', coords: [50.1051446, 8.9348691], status: 'closed', offsets: [0, -2]},
+    //         {name: 'KKS', coords: [53.6200685, 9.5306289], status: 'closed'},
+    //         {name: 'KKN', coords: [48.6558015, 12.2500848], status: 'closed', offsets: [0, -2]},
+    //         {name: 'KGR', coords: [54.1417497, 13.6583877], status: 'closed'},
+    //         {name: 'KWB', coords: [49.709331, 8.415865], status: 'closed'},
+    //         {name: 'KWW', coords: [51.6396481, 9.3915617], status: 'closed'},
+    //         {name: 'GKN', coords: [49.0401151, 9.1721088], status: 'activeUntil2022'},
+    //         {name: 'KKB', coords: [53.8913533, 9.2005777], status: 'closed', offsets: [0, -5]},
+    //         {name: 'KKI', coords: [48.5544748, 12.3472095], status: 'activeUntil2022', offsets: [0, 2]},
+    //         {name: 'KKU', coords: [53.4293465, 8.4774649], status: 'closed'},
+    //         {name: 'KNK', coords: [49.1473279, 8.3827739], status: 'closed'},
+    //         {name: 'KKP', coords: [49.2513078, 8.4356761], status: 'activeUntil2022', offsets: [0, -2]},
+    //         {name: 'KKG', coords: [49.9841308, 10.1846373], status: 'activeUntil2018'},
+    //         {name: 'KKK', coords: [53.4104656, 10.4091597], status: 'closed'},
+    //         {name: 'KWG', coords: [52.0348748, 9.4097793], status: 'activeUntil2022'},
+    //         {name: 'KBR', coords: [53.850666, 9.3457603], status: 'closed', offsets: [0, 5]},
+    //         {name: 'KMK', coords: [50.408791, 7.4861956], status: 'closed'},
+    //         {name: 'THTR', coords: [51.6786228, 7.9700232], status: 'closed'},
+    //         {name: 'KKE', coords: [52.4216974, 7.3706389], status: 'activeUntil2022', offsets: [0, 2]}
+    //     ];
+    //
+    //     new jvm.Map({
+    //         container: $('#world-map2'),
+    //         map: 'de_merc',
+    //         markers: plants.map(function (h) {
+    //             return {name: h.name, latLng: h.coords}
+    //         }),
+    //         labels: {
+    //             markers: {
+    //                 render: function (index) {
+    //                     return plants[index].name;
+    //                 },
+    //                 offsets: function (index) {
+    //                     var offset = plants[index]['offsets'] || [0, 0];
+    //
+    //                     return [offset[0] - 7, offset[1] + 3];
+    //                 }
+    //             }
+    //         },
+    //         series: {
+    //             markers: [{
+    //                 attribute: 'image',
+    //                 scale: {
+    //                     'closed': '/img/icon-np-3.png',
+    //                     'activeUntil2018': '/img/icon-np-2.png',
+    //                     'activeUntil2022': '/img/icon-np-1.png'
+    //                 },
+    //                 values: plants.reduce(function (p, c, i) {
+    //                     p[i] = c.status;
+    //                     return p
+    //                 }, {}),
+    //                 legend: {
+    //                     horizontal: true,
+    //                     title: 'Nuclear power station status',
+    //                     labelRender: function (v) {
+    //                         return {
+    //                             closed: 'Closed',
+    //                             activeUntil2018: 'Scheduled for shut down<br> before 2018',
+    //                             activeUntil2022: 'Scheduled for shut down<br> before 2022'
+    //                         }[v];
+    //                     }
+    //                 }
+    //             }]
+    //         }
+    //     });
+    //
+    //
+    //     $('#world-map').vectorMap({})
+    // }
 });
