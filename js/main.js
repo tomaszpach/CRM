248a1e7fa4d8256fba3fcbcc6b8f88686b1e320b
tomaszpach <tomaszpach@icloud.com>
2018-10-23 00:03:43 +0200

    stacked bar chart, put some data

--- js/main.js
+++ js/main.js
@@ -365,40 +365,44 @@ $(document).ready(function () {
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 25, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+            labels: ["Konrad D.", "Jacek R.", "Wioletta N.", "Radosław R.", "Małgorzata L.", "Izabela B.", "Piotr M."],
+            datasets: [
+                {
+                    label: 'Email komunikator',
+                    data: [219, 303, 238, 199, 230, 248, 138],
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                    borderColor: ['rgba(255,99,132,1)',],
+                    borderWidth: 1
+                },
+                {
+                    label: 'Telefon',
+                    data: [220, 117, 84, 72, 66, 30, 45],
+                    backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+                    borderColor: ['rgba(54, 162, 235, 1)'],
+                    borderWidth: 1
+                },
+                {
+                    label: '# of Votes',
+                    data: [12, 65, 19, 3, 5, 2, 3],
+                    backgroundColor: ['rgba(255, 206, 86, 0.2)',],
+                    borderColor: ['rgba(255, 206, 86, 1)',],
+                    borderWidth: 1
+                }]
         },
         options: {
             scales: {
+                xAxes: [{
+                    stacked: true
+                }],
                 yAxes: [{
+                    stacked: true,
                     ticks: {
-                        beginAtZero:true
+                        beginAtZero: true
                     }
                 }]
             }
         }
     });
-
 
 
     //Ricksaw Chart for Server Load - Autoupdate
