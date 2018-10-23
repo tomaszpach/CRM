25beb4aab36be7799186fd2b6a13b8123f351e3a
Tomasz Pach <tomaszpach@icloud.com>
2018-10-23 15:41:16 +0200

    mini bar chart

--- js/main.js
+++ js/main.js
@@ -360,9 +360,8 @@ $(document).ready(function () {
 
     // $.plot($("#placeholder-bar-chart"), [ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
 
-    let ctx = document.getElementById("stacked-chart");
-
-    var myChart = new Chart(ctx, {
+    let $stackedChart = document.getElementById("stacked-chart"),
+        stackedChart = new Chart($stackedChart, {
         type: 'bar',
         data: {
             labels: ["Konrad D.", "Jacek R.", "Wioletta N.", "Radosław R.", "Małgorzata L.", "Izabela B.", "Piotr M."],
@@ -426,9 +425,8 @@ $(document).ready(function () {
 
 
 
-    let lineChart = document.getElementById("line-chart");
-
-    var myChart = new Chart(lineChart, {
+    let $lineChart = document.getElementById("line-chart"),
+        lineChart = new Chart($lineChart, {
         type: 'line',
         data: {
             labels: ["Okno 1", "Okno 2", "Okno 3", "Okno 4", "Okno 5", "Okno 6", "Okno 7"],
@@ -438,14 +436,16 @@ $(document).ready(function () {
                     data: [219, 303, 238, 199, 230, 248, 138],
                     backgroundColor: 'transparent',
                     borderColor: 'rgba(255,99,132,1)',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    lineTension: 0
                 },
                 {
                     label: 'NETTO po rabatach',
                     data: [199, 287, 221, 187, 218, 216, 111],
                     backgroundColor: 'rgba(255,99,132,0.2)',
                     borderColor: 'rgba(255,99,132,1)',
-                    borderWidth: 1
+                    borderWidth: 1,
+                    lineTension: 0
                 }
             ]
         },
@@ -465,43 +465,53 @@ $(document).ready(function () {
     });
 
 
-    let miniBarChart = document.getElementById("mini-chart-balance");
+    // let $miniBarChart = document.getElementById("mini-chart-balance"),
+    //     miniBarChart = new Chart($miniBarChart, {
+    //     type: 'bar',
+    //     data: {
+    //         labels: ["Okno 1", "Okno 2", "Okno 3", "Okno 4", "Okno 5", "Okno 6", "Okno 7"],
+    //         datasets: [
+    //             {
+    //                 label: 'Balance',
+    //                 data: [20, 16, 17, 12, 12, 32, 27],
+    //                 backgroundColor: 'transparent',
+    //                 borderColor: 'rgba(255,99,132,1)',
+    //                 borderWidth: 1,
+    //                 lineTension: 0
+    //             },
+    //
+    //             // {
+    //             //     label: 'NETTO po rabatach',
+    //             //     data: [199, 287, 221, 187, 218, 216, 111],
+    //             //     backgroundColor: 'rgba(255,99,132,0.2)',
+    //             //     borderColor: 'rgba(255,99,132,1)',
+    //             //     borderWidth: 1
+    //             // }
+    //         ]
+    //     },
+    //     options: {
+    //         scales: {
+    //             xAxes: [{
+    //                 stacked: false
+    //             }],
+    //             yAxes: [{
+    //                 display: false,
+    //                 stacked: false,
+    //                 ticks: {
+    //                     beginAtZero: true
+    //                 }
+    //             }]
+    //         }
+    //     }
+    // });
 
-    var myChart = new Chart(miniBarChart, {
+    $("#mini-chart-balance2").sparkline([1, 4, 6, 2, 0, 5, 6, 4], {
         type: 'bar',
-        data: {
-            labels: ["Okno 1", "Okno 2", "Okno 3", "Okno 4", "Okno 5", "Okno 6", "Okno 7"],
-            datasets: [
-                {
-                    label: 'Balance',
-                    data: [20, 16, 17, 12, 12, 32, 27],
-                    backgroundColor: 'transparent',
-                    borderColor: 'rgba(255,99,132,1)',
-                    borderWidth: 1
-                },
-                // {
-                //     label: 'NETTO po rabatach',
-                //     data: [199, 287, 221, 187, 218, 216, 111],
-                //     backgroundColor: 'rgba(255,99,132,0.2)',
-                //     borderColor: 'rgba(255,99,132,1)',
-                //     borderWidth: 1
-                // }
-            ]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    stacked: false,
-                    barThickness: 30
-                }],
-                yAxes: [{
-                    stacked: false,
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+        height: '30px',
+        barWidth: 6,
+        barSpacing: 2,
+        barColor: 'rgba(255,99,132,0.7)',
+        negBarColor: '#0aa699'
     });
 
 
