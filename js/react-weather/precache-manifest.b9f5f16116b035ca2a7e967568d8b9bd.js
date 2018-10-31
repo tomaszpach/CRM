bb5b9ccbe8a1e5f4189ff57ab1a219270a5f81d2
Tomasz Pach <tomaszpach@icloud.com>
2018-10-31 14:49:34 +0100

    add weather component

---
+++ js/react-weather/precache-manifest.b9f5f16116b035ca2a7e967568d8b9bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fad5f6d26f329878da8",
+    "url": "/static/js/main.7fad5f6d.chunk.js"
+  },
+  {
+    "revision": "5993bd44acefd3a350e2",
+    "url": "/static/js/1.5993bd44.chunk.js"
+  },
+  {
+    "revision": "7fad5f6d26f329878da8",
+    "url": "/static/css/main.5308eabc.chunk.css"
+  },
+  {
+    "revision": "1c58efa2a1e4b94a7618bf59cf2f2a5c",
+    "url": "/index.html"
+  }
+];
