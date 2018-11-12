e387191c6a3bc5b5a0990c1180783d800ef0b263
Tomasz Pach <tomaszpach@icloud.com>
2018-11-12 11:05:19 +0100

    restore sidebar, updated weather widget, index cleanup

---
+++ js/react-weather/precache-manifest.bec2c43d5fea4f78aaf1328900d4367f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1b7d3c43d8338a67d57e",
+    "url": "./static/js/main.1b7d3c43.chunk.js"
+  },
+  {
+    "revision": "b1ff0e5434e62003d014",
+    "url": "./static/js/1.b1ff0e54.chunk.js"
+  },
+  {
+    "revision": "1b7d3c43d8338a67d57e",
+    "url": "./static/css/main.daa1b83b.chunk.css"
+  },
+  {
+    "revision": "3d98063e09d935d509ebfd403ddbaa87",
+    "url": "./index.html"
+  }
+];
